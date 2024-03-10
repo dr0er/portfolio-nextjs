563ed9804f8508677606773ba12d0235d1f90bc6
dr0er <ernextxxx123@gmail.com>
2024-03-10 19:08:24 +0100

    add skills section change

--- components/skills.tsx
+++ components/skills.tsx
@@ -29,7 +29,7 @@ export default function Skills() {
       ref={ref}
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
     >
-      <SectionHeading>My skills</SectionHeading>
+      <SectionHeading>Highlights of my toolkit 🛠️</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
           <motion.li

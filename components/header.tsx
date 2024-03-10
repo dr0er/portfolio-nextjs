f18ec085232490fccf47c8af42e5f26e3f842aec
dr0er <ernextxxx123@gmail.com>
2024-03-10 20:45:27 +0100

    add header adjustments for more routes

--- components/header.tsx
+++ components/header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
             >
               <Link
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  "flex w-full items-center justify-center px-1 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
                   {
                     "text-gray-950 dark:text-gray-200":
                       activeSection === link.name,

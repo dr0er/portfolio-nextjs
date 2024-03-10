4249b403df365749473d788b44cab8df0cbf2df1
dr0er <ernextxxx123@gmail.com>
2024-03-10 19:08:41 +0100

    add projects section change

--- components/projects.tsx
+++ components/projects.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>My projects</SectionHeading>
+      <SectionHeading>Selected works 🫱🏼‍🫲🏼</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>

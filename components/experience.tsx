48648afb2c2a1a10137ff371f56d825dd42b44dc
dr0er <ernextxxx123@gmail.com>
2024-03-10 19:08:06 +0100

    add exp section change

--- components/experience.tsx
+++ components/experience.tsx
@@ -17,7 +17,7 @@ export default function Experience() {
 
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
-      <SectionHeading>My experience</SectionHeading>
+      <SectionHeading>Where did I gain experience? 🤔</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>

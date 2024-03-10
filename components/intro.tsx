0c169df3fe1adaff64cd26aad98540c227cf1f21
dr0er <ernextxxx123@gmail.com>
2024-03-10 20:45:43 +0100

    add social link hrefs

--- components/intro.tsx
+++ components/intro.tsx
@@ -102,7 +102,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/ernest-drobny/"
           target="_blank"
         >
           <BsLinkedin />
@@ -110,7 +110,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/dr0er"
           target="_blank"
         >
           <FaGithubSquare />

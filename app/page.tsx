72962f8e43a536ae2c0d70fc6d19c5844b02822a
dr0er <ernextxxx123@gmail.com>
2024-03-10 20:46:17 +0100

    add hash appropriate order

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/about";
 import Contact from "@/components/contact";
+import Education from "@/components/education";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
@@ -12,9 +13,10 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
-      <Projects />
       <Skills />
+      <Projects />
       <Experience />
+      <Education />
       <Contact />
     </main>
   );

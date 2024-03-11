ec89cf2d54c2efd817ada193cc8fd3f6fa879533
dr0er <ernextxxx123@gmail.com>
2024-03-11 17:13:20 +0100

    add theme switcher fix

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -13,13 +13,13 @@ export default function ThemeSwitch() {
     setClicked(true);
   };
 
-  const buttonClassName = `fixed bottom-5 right-5 bg-white w-[3.5rem] h-[3.5rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-dashed border-red-600 border-x-[3px] border-y-[3px] border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:bg-red-200 hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 ${
+  const buttonClassName = `fixed bottom-5 right-5 bg-white w-[3.5rem] h-[3.5rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-gray-400 border-x-[2px] border-y-[2px] border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:bg-red-200 hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 ${
     clicked ? "" : "animate-bounce"
   }`;
 
   return (
     <button className={buttonClassName} onClick={handleClick}>
-      {theme === "light" ? <BsSun /> : <BsMoon />}
+      {theme === "light" ? <BsMoon /> : <BsSun />}
     </button>
   );
 }

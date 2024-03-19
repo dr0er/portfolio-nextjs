59edae43da6ee8ace98350a5389570a51e157079
dr0er <ernextxxx123@gmail.com>
2024-03-20 00:31:37 +0100

    fix CV no-show issue

--- components/intro.tsx
+++ components/intro.tsx
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 mr-2 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 hover:bg-red-200"
-          href="/DROBNY_ERNEST_CV.pdf"
+          href="/CV.pdf"
           download
         >
           Download CV{" "}

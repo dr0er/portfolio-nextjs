a31f908704b7e4f2ca787acf6588abc027b218ac
dr0er <ernextxxx123@gmail.com>
2024-03-28 13:53:06 +0100

    add quick console icon error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,11 +34,7 @@ export const metadata = {
     icon: [
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/icon.png" },
-      new URL("/icon.png", "https://example.com"),
-      { url: "/icon-dark.png", media: "(prefers-color-scheme: dark)" },
     ],
-    shortcut: ["/shortcut-icon.png"],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180" },
       { url: "/apple-icon-x3.png", sizes: "180x180", type: "image/png" },

83e6e39f2a7d6e0950132bbe2fdcc4b00511f1fd
dr0er <ernextxxx123@gmail.com>
2024-06-02 22:09:48 +0200

    vertical timeline doesnt work after upgrading to next 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,28 +9,28 @@ import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "E. Drobny | Personal Website",
-  description: "Web developer ready to solve your business problems.",
-  icons: {
-    icon: [
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180" },
-      { url: "/apple-icon-x3.png", sizes: "180x180", type: "image/png" },
-      {
-        rel: "apple-touch-icon-precomposed",
-        url: "/apple-touch-icon-precomposed.png",
-      },
-    ],
-  },
-  openGraph: {
-    images: "/opengraph-image.png",
-  },
-  links: [{ rel: "manifest", href: "/site.webmanifest" }],
-};
+// export const metadata = {
+//   title: "E. Drobny | Personal Website",
+//   description: "Web developer ready to solve your business problems.",
+//   icons: {
+//     icon: [
+//       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+//       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+//     ],
+//     apple: [
+//       { url: "/apple-touch-icon.png", sizes: "180x180" },
+//       { url: "/apple-icon-x3.png", sizes: "180x180", type: "image/png" },
+//       {
+//         rel: "apple-touch-icon-precomposed",
+//         url: "/apple-touch-icon-precomposed.png",
+//       },
+//     ],
+//   },
+//   openGraph: {
+//     images: "/opengraph-image.png",
+//   },
+//   links: [{ rel: "manifest", href: "/site.webmanifest" }],
+// };
 
 export default function RootLayout({
   children,
